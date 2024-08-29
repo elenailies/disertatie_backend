a86f4302e2cb6c3d86b350507971633e85c70efb
ILIES ELENA <elenailies90@yahoo.com>
2024-08-30 00:32:10 +0300

    cmt

---
+++ disertatie/src/main/java/com/example/disertatie/resource/EmailController.java
@@ -0,0 +1,30 @@
+package com.example.disertatie.resource;
+
+import com.example.disertatie.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.disertatie.model.Destination;
+import com.example.disertatie.service.DestinationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
+@RequestMapping("/email")
+public class EmailController {
+
+    @Autowired
+    private EmailService emailService;
+
+    @GetMapping("/send")
+    public String sendEmail() {
+        emailService.sendSimpleMessage("elenailies09@gmail.com", "Test Subject", "Test email body");
+        return "Email sent!";
+    }
+}
+
