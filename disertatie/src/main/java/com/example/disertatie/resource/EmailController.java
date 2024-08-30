e74bb5a3840ea8b76a86f6d94e740678b2cad146
ILIES ELENA <elenailies90@yahoo.com>
2024-08-30 21:14:23 +0300

    cmt

--- disertatie/src/main/java/com/example/disertatie/resource/EmailController.java
+++ disertatie/src/main/java/com/example/disertatie/resource/EmailController.java
@@ -3,15 +3,15 @@ package com.example.disertatie.resource;
 import com.example.disertatie.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.disertatie.model.Destination;
-import com.example.disertatie.service.DestinationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
@@ -21,10 +21,19 @@ public class EmailController {
     @Autowired
     private EmailService emailService;
 
-    @GetMapping("/send")
-    public String sendEmail() {
-        emailService.sendSimpleMessage("elenailies09@gmail.com", "Test Subject", "Test email body");
-        return "Email sent!";
-    }
-}
+    @PostMapping("/send")
+    public ResponseEntity<Map<String, String>> sendEmail(@RequestBody Map<String, String> emailDetails) {
+        String to = emailDetails.get("to");
+        String subject = emailDetails.get("subject");
+        String text = emailDetails.get("text");
 
+        emailService.sendSimpleMessage(to, subject, text);
+
+        // Create a JSON response
+        Map<String, String> response = new HashMap<>();
+        response.put("message", "Email sent successfully!");
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+}
